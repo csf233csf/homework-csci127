3b84166dfcb3ad0a365518b52f9dff783fb6ac19
csf233csf <2452864281x@gmail.com>
2024-09-17 12:05:59 -0400

    hw

---
+++ upperlower.py
@@ -0,0 +1,10 @@
+#Name:  Shifan Chen
+#Email:  SHIFAN.CHEN25@myhunter.cuny.edu
+#Date: Septmeber 17, 2024
+#This programs takes user input and prints it in in original, upper, and lower format.
+
+String = input('Enter a message: ')
+
+print(String)
+print(String.upper())
+print(String.lower())
